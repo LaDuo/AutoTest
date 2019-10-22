e3c236c09b7f54f104ab672905d3bc6ae6884d98
LaDuo <34500165+LaDuo@users.noreply.github.com>
2019-10-22 11:25:12 +0800

    update
    update defines

--- defi.py
+++ defi.py
@@ -1,6 +1,12 @@
 import re
 import os
+import shutil
+import zipfile
+from datetime import datetime
+
 # 此文件用于添加定义、变量
 Desktop = os.path.join(os.path.expanduser('~'), "Desktop")
 Av2_log = Desktop + "\\Av2_Output\\outfile.txt"
-
+Av2_Input = Desktop + "\\Av2_Input"
+Av2_Output = Desktop + "\\Av2_Output"
+Analyze = Desktop + "\\Analyze"
