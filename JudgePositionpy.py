ab92b028ed86c61eaa36008008be0c4aa517ac71
LaDuo <34500165+LaDuo@users.noreply.github.com>
2019-10-23 23:52:44 +0800

    Add files via upload
    更新Judge脚本

--- JudgePositionpy.py
+++ JudgePositionpy.py
@@ -1,35 +1,74 @@
 import re
 import os
+from tkinter import *
 
-# from defi import *
+
+# 此文件用于添加定义、变量
+Desktop = os.path.join(os.path.expanduser('~'), "Desktop")
+Av2_log = Desktop + "\\Av2_Output\\outfile.txt"
+Av2_Input = Desktop + "\\Av2_Input"
+Av2_Output = Desktop + "\\Av2_Output"
+Analyze = Desktop + "\\Analyze"
+base_path1 = "D:\\test\\Release\\log\\"  # Av2数据所在路径
+
+list_ehp = []
+list_ada = []
+list_loc = []
+
 
 Desktop = os.path.join(os.path.expanduser('~'), "Desktop")
 pos = Desktop + "\\pos.txt"
+Err_Position = Desktop + "\\Err.txt"
 
 with open(pos, 'r', encoding="utf-8") as file:
     tmp = file.readlines()
 
-pattern_position = re.compile(r'Position: path=\d+, ofs=\d+, speed=\d+')
+position = re.compile(r'\d+-\d+ \d+:\d+:\d+\.\d+')
+pattern_ofs = re.compile(r'ofs=\d+,')
+pattern_path = re.compile(r'path=\d+,')
+pattern_speed = re.compile(r'speed=\d+,')
+Err = []
 
 
 def JudgePosition():
+    flag = True
     for i in range(len(tmp)):
         if i == len(tmp) - 1:
             break
         j = i + 1
-        fir = pattern_position.findall(tmp[i])
-        las = pattern_position.findall(tmp[j])
-        str_fir = str(fir)
-        str_las = str(las)
-        print(str_fir)
-        speed1 = str_fir[35:37]
-        speed2 = str_las[35:37]
-        ofs1 = str_fir[25:27]
-        ofs2 = str_las[25:27]
-        path1 = str_fir[17:19]
-        path2 = str_las[17:19]
+        time = position.findall(tmp[i])
+        speed1 = str(pattern_speed.findall(tmp[i]))
+        speed2 = str(pattern_speed.findall(tmp[j]))
+        ofs1 = str(pattern_ofs.findall(tmp[i]))
+        ofs2 = str(pattern_ofs.findall(tmp[j]))
+        path1 = str(pattern_path.findall(tmp[i]))
+        path2 = str(pattern_path.findall(tmp[j]))
+        speed1 = re.sub(r'\D', "", speed1)
+        speed2 = re.sub(r'\D', "", speed2)
+        ofs1 = re.sub(r'\D', "", ofs1)
+        ofs2 = re.sub(r'\D', "", ofs2)
+        path1 = re.sub(r'\D', "", path1)
+        path2 = re.sub(r'\D', "", path2)
+        if path1 == 0:
+            continue
+        if ofs2 > ofs1 and path1 == path2:
+            print("HAHAHAHAHAHAHAHHA", ofs2, ofs1)
+            flag = True
+        elif ofs2 == ofs1 and speed1 == 0 and speed2 == 0:
+            flag = True
+        elif ofs2 < ofs1 and path1 != path2:
+            flag = True
+        elif ofs2 < ofs1 and path1 == path2:
+            Err.append(str(tmp[i]))
+            Err.append(str(tmp[j]))
+            print(time)
+            print(ofs2, ofs1, speed2, speed1, path2, path1)
+
 
 
 if __name__ == '__main__':
     JudgePosition()
-
+    with open(Err_Position, 'w', encoding="utf-8") as file:
+        for i in range(len(Err)):
+            file.write(str(Err[i]))
+            file.write("\n")
